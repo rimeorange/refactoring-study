81183dedd2b10f2d66bf3bc9cbfc6ae791ab2741
rimeorange <rimeorange83@gmail.com>
2021-07-21 23:15:39 +0900

    Chapter 4.2.3 리팩토링후 : NullLabel 클래스 추가.

---
+++ src/IntroduceNullObject/after/NullLabel.java
@@ -0,0 +1,16 @@
+package IntroduceNullObject.after;
+
+public class NullLabel extends Label{
+    public NullLabel() {
+        super("(none)");
+    }
+
+    @Override
+    public boolean isNull() {
+        return true;
+    }
+
+    @Override
+    public void display() {
+    }
+}
---
+++ src/IntroduceNullObject/after/Person.java
@@ -0,0 +1,24 @@
+package IntroduceNullObject.after;
+
+public class Person {
+    private final Label _name;
+    private final Label _mail;
+
+    public Person(Label _name, Label _mail) {
+        this._name = _name;
+        this._mail = _mail;
+    }
+
+    public Person(Label name) {
+        this(name, new NullLabel());
+    }
+
+    public void display() {
+        _name.display();
+        _mail.display();
+    }
+
+    public String toString() {
+        return "[ Person: name=" + _name + " mail=" + _mail + " ]";
+    }
+}
